cf84fea89529470f506029fe38fe6ae438251c7e
Mauricio <rezamar555@gmail.com>
2022-05-01 17:53:13 -0500

    Informative Comments

--- helloworld.bat
+++ helloworld.bat
@@ -1,3 +1,7 @@
-@echo OFF
+@ECHO OFF
+::Mreyes Apr, 2022
+::@ECHO OFF = Shows message in a clean line
 ECHO Hello World.
+::ECHO = Prints text after space
+::PAUSE = allow the window to stay open when batch has been executed
 PAUSE
