65cbbadf015d3ca8c1a34ccb9fef5064f18d1aa2
Mauricio <rezamar555@gmail.com>
2022-05-02 12:10:47 -0500

    Adding gitmanage automation script

---
+++ gitmanage.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+::Mauricio Reye April, 2022
+::This script push automatically changes to origin remotes at end of day
+SET path=C:\Users\maury\Documents\testing\Cpp
+SET gitl=gitlab
+SET gith=github
+
+cd
