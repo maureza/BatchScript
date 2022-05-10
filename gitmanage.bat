1230dfd70af6ec146373092709cf74b9764e067b
Mauricio <rezamar555@gmail.com>
2022-05-10 00:53:33 -0500

    Finished: git invocation via batch

--- gitmanage.bat
+++ gitmanage.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 ::Mauricio Reye April, 2022
 ::This script push automatically changes to origin remotes at end of day
-SET path=C:\Users\maury\Documents\testing\Cpp
-SET gitl=gitlab
-SET gith=github
-
-cd
+SET PATHCPP=C:\Users\maury\Documents\testing\Cpp
+SET PATHSCRIPT=C:Users\maury\Documents\testing\scripts
+::SET GIT_PATH="C:\Program Files (x86)\Git\bin\git.exe" -- IGNORE THIS
+::SET BRANCH="master" -- AND THIS
+::cd %PATHFILES% -- AND ALSO THIS
+CALL git status
+PAUSE
