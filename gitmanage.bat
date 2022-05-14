6e678984c6a4a660304c168fc8de6f06d47567ba
Mauricio <rezamar555@gmail.com>
2022-05-14 00:01:20 -0500

    Finished git status in definitive path

--- gitmanage.bat
+++ gitmanage.bat
@@ -1,10 +1,6 @@
 @ECHO OFF
-::Mauricio Reye April, 2022
-::This script push automatically changes to origin remotes at end of day
-SET PATHCPP=C:\Users\maury\Documents\testing\Cpp
-SET PATHSCRIPT=C:Users\maury\Documents\testing\scripts
-::SET GIT_PATH="C:\Program Files (x86)\Git\bin\git.exe" -- IGNORE THIS
-::SET BRANCH="master" -- AND THIS
-::cd %PATHFILES% -- AND ALSO THIS
-CALL git status
+REM Mauricio Reye April, 2022
+REM script push automatically changes to origin remotes at end of day
+REM ---------------------------------Author Comments
+git --git-dir=<PATHPOINTED>\.git --work-tree=<PATHPOINTED> status
 PAUSE
