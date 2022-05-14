6e642ce4a5fe19e3a40160c4b51d7a8a76f0233e
Mauricio <rezamar555@gmail.com>
2022-05-14 00:21:20 -0500

    Encapsuled command in a batch variable | Added informative comment

--- gitmanage.bat
+++ gitmanage.bat
@@ -1,6 +1,9 @@
 @ECHO OFF
 REM Mauricio Reye April, 2022
 REM script push automatically changes to origin remotes at end of day
-REM ---------------------------------Author Comments
-git --git-dir=<PATHPOINTED>\.git --work-tree=<PATHPOINTED> status
+REM ---------------------------------Author Comments^
+SET gitcommand=git --git-dir=<PATHPOINTED>\.git --work-tree=<PATHPOINTED> status
+REM ---------------------------------Global Variable^
+CALL %gitcommand%
+REM ---------------------------------Main process^
 PAUSE
